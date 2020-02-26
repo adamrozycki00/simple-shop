ab5a36e7d140f4b0bac69ba7db6515415e0948a2
Adam Rozycki <adamrozycki00@gmail.com>
2020-02-26 19:16:31 +0100

    init

---
+++ src/test/java/pl/adaroz/springboot2/homework2/ApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adaroz.springboot2.homework2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
